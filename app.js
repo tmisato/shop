42bd1c35c07c5ba024db77a2c6dc0fa074373a14
tmisato <1nr7007m@gmail.com>
2023-06-27 17:39:46 +0900

    m index.ejs追加

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs'); //追加
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs');
 
 const mysql = require('mysql2');
 const con = mysql.createConnection({
@@ -9,6 +14,7 @@ const con = mysql.createConnection({
   password: 'rootroot',
   database: 'shop_db' //接続オプションの追加
 });
+
 
 
 
